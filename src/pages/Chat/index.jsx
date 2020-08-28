f648dcb398b150e8ea6ca48ed9b46896fde629c5
Hillel Saal <remi.saal@gmail.com>
2020-08-28 14:46:25 +0000

    Fixed chat

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,26 +1,18 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
-import socketIO from "socket.io-client"
+import io from "socket.io-client"
 
 import styles from "./style.scss"
 
-const ENDP = "//localhost:3000"
+const socket = io("http://localhost:3000")
 
 const Chat = () => {
-
-    useEffect(() => {
-        var socket = socketIO.connect(ENDP)
-        socket.on('c', () => { "C" })
-    }, [])
-
-    return (
-        <div className={styles.container}>
-            <h1 className={styles.title}>Chat</h1>
-            <div className={styles["form-group"]}>
-
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Chat</h1>
+      <div className={styles["form-group"]}></div>
+    </div>
+  )
 }
 
 export default Chat
