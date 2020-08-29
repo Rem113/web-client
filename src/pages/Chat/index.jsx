45371b5718cbef6246825f0e40ac47de04bb6459
Gabriel <gelbaz@g.jct.ac.il>
2020-08-29 22:22:59 +0300

    Start using SocketIO

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -4,15 +4,19 @@ import io from "socket.io-client"
 
 import styles from "./style.scss"
 
-const socket = io("http://localhost:3000")
-
 const Chat = () => {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Chat</h1>
-      <div className={styles["form-group"]}></div>
-    </div>
-  )
+
+    useEffect(() => {
+        const socket = io("http://localhost:3000")
+        socket.emit('isManager', `Je suis ${localStorage.getItem('isManager')}`)
+    }, [])
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Chat</h1>
+            <div className={styles["form-group"]}></div>
+        </div>
+    )
 }
 
 export default Chat
